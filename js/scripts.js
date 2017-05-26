c18a25e2016411d8523f5e77fc1c864d4d5d9898
rafal <ra88fi@o2.pl>
2017-05-26 21:06:42 +0200

    Comment in script.js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // scripts.js czyli pierwszy skrypt w JS
 
+//Jest to zadanie 8.4 - dodawanie komentarzy a to jest komentarz linowy
+
 var name = prompt("Wpisz swoje imię");
-alert("Siema, " + name);
+// alert("Siema, " + name);
 console.log("Siema, " + name);
+
+/* To natomiast jest komentarz blokowy
+ bo można tu sobie wpisywać
+ dużo
+ fajnego
+ kodu
+ w postaci wielu linii 
+ */
